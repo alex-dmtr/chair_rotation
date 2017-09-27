df5e3c9c6d21688de1745850db5c93592cb8b73b
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-09-27 10:07:39 +0100

    Fix randomness

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import json
 from random import randint
 
-with open('data.json', 'r') as data_file:    
-    people = json.load(data_file)['people']
+def get_data():
+    with open('data.json', 'r') as data_file:    
+        people = json.load(data_file)['people']
 
-try:
-    with open('history.json', 'r') as data_file:
-        history = json.load(data_file)['history']
-except:
-    history = []
+    try:
+        with open('history.json', 'r') as data_file:
+            history = json.load(data_file)['history']
+    except:
+        history = []
     
-history = history[-len(people)+1:]
-people_pool = [p for p in people if not p['id'] in history]
+    return (people, history)
 
-indx = randint(0, len(people_pool)-1)
+def pick_chair(people, history):
+    if len(history) >= len(people):
+        history = []
 
-person = people_pool[indx]
+    people_pool = [p for p in people if not p['id'] in history]
 
-print("Next chair is: %s" % person['name'])
+    indx = randint(0, len(people_pool)-1)
 
-history.append(person['id'])
+    person = people_pool[indx]
 
-with open('history.json', 'w') as file:
-    file.write(json.dumps({"history":history}))
-# pprint(data)
+    history.append(person['id'])
+
+    return person, history
+
+def save_history(history):
+    with open('history.json', 'w') as file:
+        file.write(json.dumps({"history":history}))
+
+if __name__ == "__main__":
+    (people, history) = get_data()
+    (person, history) = pick_chair(people, history)
+    print("Next chair is: %s" % person['name'])
+
+    save_history(history)

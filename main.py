7d59ce83889d6ff0916bda340dcc1e385af54ce9
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-09-28 22:06:49 +0100

    Show both chair and scribe

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ def get_data():
     with open('data.json', 'r') as data_file:    
         people = json.load(data_file)['people']
 
-    try:
-        with open('history.json', 'r') as data_file:
-            history = json.load(data_file)['history']
-    except:
-        history = []
+    # try:
+    #     with open('history.json', 'r') as data_file:
+    #         history = json.load(data_file)['history']
+    # except:
+    #     history = []
     
+    history = []
     return (people, history)
 
-def pick_chair(people, history):
-    if len(history) >= len(people):
+def pick_person(people, history, remove_id=None):
+    if len(history) >= len(people) or (
+        not remove_id is None or len(people) - len(history) == 1
+                               and remove_id in history):
         history = []
 
-    people_pool = [p for p in people if not p['id'] in history]
+    people_pool = [p for p in people 
+                        if not p['id'] in history 
+                           and (remove_id is None
+                                or not p['id'] == remove_id)
+                                ]
 
+    # print(people_pool)
     indx = randint(0, len(people_pool)-1)
 
     person = people_pool[indx]
@@ -31,9 +39,21 @@ def save_history(history):
     with open('history.json', 'w') as file:
         file.write(json.dumps({"history":history}))
 
-if __name__ == "__main__":
-    (people, history) = get_data()
-    (person, history) = pick_chair(people, history)
-    print("Next chair is: %s" % person['name'])
+def main():
+    people, history = get_data()
 
+    picks = []
+    for i in range(0, 12):
+        person1, history = pick_person(people, history)
+        person2, history = pick_person(people, history)
+        picks.append((person1, person2))
+
+    # print([p['name'] for p in picks])
+
+    for i in range(0, len(picks)):
+        print('Week %d:\n  Chair:\t%s\n  Scribe:\t%s' % 
+            (i+1, picks[i][0]['name'], picks[i][1]['name']))
     save_history(history)
+    
+if __name__ == "__main__":
+    main()
